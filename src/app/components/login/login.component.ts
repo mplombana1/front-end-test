f648b37813f317de6ac3369f5671a3070a342df7
matthew <mplombana@gmail.com>
2019-10-22 21:45:09 -0400

    adding products routes

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -15,14 +15,17 @@ export class LoginComponent implements OnInit {
   isUserLoggedIn: boolean = false;
   response: any;
 
+  usernameControl:boolean = false;
+  passwordControl:boolean = false;
+
   constructor(
     private fb: FormBuilder,
     private login: LoginService,
     private router: Router
   ) {
     this.form = this.fb.group({
-      username: [""],
-      password: [""]
+      username: [''],
+      password: ['']
     });
   }
 
@@ -30,6 +33,12 @@ export class LoginComponent implements OnInit {
   
   submit() {
     const val = this.form.value;
+    if (val.username.length === 0){
+      this.usernameControl = true;
+    }
+    if(val.password.length === 0 ){
+      this.passwordControl = true;
+    }
     if (val.username && val.password) {
       this.login.getAuth(val.username, val.password).subscribe(res => {
         console.log(res, "res");
